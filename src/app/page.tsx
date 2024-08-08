fc16e858c62f9450e261ffcf3b3f6bedd17315c5
toadmilk <48373163+toadmilk@users.noreply.github.com>
2024-08-08 17:41:01 +1200

    Reverted gradient addition as they are too laggy.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,21 +18,6 @@ const BLUR_FADE_DELAY = 0.04;
 export default function Page() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
-      {/* Really nice gradient blurred shapes to add colour to the page */}
-      <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-400 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-800">
-        <div style={{
-          clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-        }} className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#FF00FE] to-[#1000FF] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-        />
-      </div>
-
-      <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-400 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-800">
-        <div style={{
-          clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-        }} className="relative left-[calc(50%-13rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#FF00FE] to-[#1000FF] opacity-30 sm:left-[calc(50%-36rem)] sm:w-[72.1875rem]"
-        />
-      </div>
-
       <section id="hero">
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
